2d50babe628f84af4924dcecc4bd9e2142ce81b7
Robin Riclet <robinriclet@gmail.com>
2016-11-30 11:44:18 +0100

    Dirty sample code

--- app/src/main/java/carnetdepeche/istic/com/carnetdepeche/HomeActivity.java
+++ app/src/main/java/carnetdepeche/istic/com/carnetdepeche/HomeActivity.java
@@ -5,9 +5,70 @@ import android.support.v7.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+        
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_home);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        // Initialize Firebase Auth and Database Reference
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        if (mFirebaseUser == null) {
+            // Not logged in, launch the Log In activity
+            loadLogInView();
+        } else {
+            mUserId = mFirebaseUser.getUid();
+
+            // Set up ListView
+            final ListView listView = (ListView) findViewById(R.id.listView);
+            final ArrayAdapter<string> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+            listView.setAdapter(adapter);
+
+            // Add items via the Button and EditText at the bottom of the view.
+            final EditText text = (EditText) findViewById(R.id.todoText);
+            final Button button = (Button) findViewById(R.id.addButton);
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    mDatabase.child("users").child(mUserId).child("items").push().child("title").setValue(text.getText().toString());
+                    text.setText("");
+                }
+            });
+
+            // Use Firebase to populate the list.
+            mDatabase.child("users").child(mUserId).child("items").addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    adapter.add((String) dataSnapshot.child("title").getValue());
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    adapter.remove((String) dataSnapshot.child("title").getValue());
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
+
 }
